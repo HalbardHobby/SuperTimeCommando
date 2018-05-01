6a20541eb2341f9ca461a6431edd7df27f279683
Alejandro Espinosa <ra.becerra388@uniandes.edu.co>
2018-04-30 23:18:10 -0500

    Nueva Implementación basada en LineTracing

--- Source/SuperTimeCommando/EnemyPatrol/EnemyPatrol.cpp
+++ Source/SuperTimeCommando/EnemyPatrol/EnemyPatrol.cpp
@@ -11,8 +11,7 @@
 #include "Materials/Material.h"
 #include "Engine/World.h"
 #include "Engine/Engine.h"
-#include "EnvironmentQuery/EnvQueryManager.h"
-#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "DrawDebugHelpers.h"
 
 
 // Sets default values
@@ -47,14 +46,39 @@ void AEnemyPatrol::BeginPlay()
 {
 	Super::BeginPlay();
 	CreateVisionCone();
-	VisionConeRequest = FEnvQueryRequest(VisionConeQuery, this);
 }
 
 // Called every frame
 void AEnemyPatrol::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	VisionConeRequest.Execute(EEnvQueryRunMode::AllMatching, this, &AEnemyPatrol::UpdateVisionCone);
+
+	FHitResult OutHit;
+	FCollisionQueryParams params;
+
+	FVector Start = GetActorLocation();
+	Start.Z += 70.f;
+
+	FVector End = ((GetActorForwardVector() * 500.f) + Start);
+
+	float DeltaVision = VisionConeAngle / (NumberOfReferencePoints - 1);
+	float StartingAngle = -VisionConeAngle / 2;
+	FRotator CurrentRotation = FRotator(0.f, StartingAngle, 0.f);
+	for (uint16 i = 1; i <= NumberOfReferencePoints; i++)
+	{
+		End = CurrentRotation.RotateVector((GetActorForwardVector() * MaxVisionDistance)) + Start;
+		CurrentRotation.Yaw += DeltaVision;
+
+		if (GetWorld()->LineTraceSingleByChannel(OutHit, Start, End, ECollisionChannel::ECC_Visibility))
+		{
+			VisionConeVertices[i] = OutHit.Location - Start;
+		}
+		else 
+		{
+			VisionConeVertices[i] = End - Start;
+		}
+	}
+	UpdateVisionCone();
 }
 
 void AEnemyPatrol::CreateVisionCone() 
@@ -63,32 +87,34 @@ void AEnemyPatrol::CreateVisionCone()
 	{
 		// El cono es un abanico partiendo del actor
 		VisionConeTriangles.Add(0);
-		VisionConeTriangles.Add(i + 1);
 		VisionConeTriangles.Add(i + 2);
+		VisionConeTriangles.Add(i + 1);
 	}
 
 	//Inicializar las demás propiedades del mesh
+	VisionConeVertices.Init(FVector(0.f, 0.f, 0.f), NumberOfReferencePoints + 1);
 	VisionConeNormals.Init(FVector(0.f, 0.f, 1.f), NumberOfReferencePoints + 1);
 	VisionConeUV.Init(FVector2D(1.f, 1.f), NumberOfReferencePoints + 1);
 	VisionConeUV[0] = FVector2D(0.f, 0.f);
 	VisionConeColor.Init(FColor(), NumberOfReferencePoints + 1);
 	VisionConeTangents.Init(FProcMeshTangent(0, 1, 0), NumberOfReferencePoints + 1);
-}
-
-void AEnemyPatrol::UpdateVisionCone(TSharedPtr<FEnvQueryResult> Result)
-{
-	Result->GetAllAsLocations(VisionConeVertices);
-	FVector ActorLocation = GetActorLocation();
-
-	// Se cambia el marco de referencia por uno local
-	for (uint16 i = 0; i < VisionConeVertices.Num(); i++)
-		VisionConeVertices[i] = VisionConeVertices[i] - ActorLocation;
-
-	// Se añade el punto de origen
-	VisionConeVertices.Insert(FVector(0.f, 0.f, 0.f), 0);
 
 	const TArray<FVector> Vertices = VisionConeVertices;
 	const TArray<int> Triangles = VisionConeTriangles;
+	const TArray<FVector> Normals = VisionConeNormals;
+	const TArray<FVector2D> UV0 = VisionConeUV;
+	const TArray<FColor> Color = VisionConeColor;
+	const TArray<FProcMeshTangent> Tangents = VisionConeTangents;
+
+	VisionConeMesh->CreateMeshSection(0, Vertices, Triangles, Normals, UV0, Color, Tangents, false);
+	VisionConeMesh->SetMaterial(0, VisionConeMaterial);
+	VisionConeMesh->SetWorldRotation(FRotator(0.f, 0.f, 0.f));
+}
+
+void AEnemyPatrol::UpdateVisionCone()
+{
+	const TArray<int> Triangles = VisionConeTriangles;
+	const TArray<FVector> Vertices = VisionConeVertices;
 	const TArray<FVector> Normals = VisionConeNormals;
 	const TArray<FVector2D> UV0 = VisionConeUV;
 	const TArray<FColor> Color = VisionConeColor;
